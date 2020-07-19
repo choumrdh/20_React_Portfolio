76db6a8f97c08c83f9859bf8efaee2c23a5f5cad
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-19 15:01:06 -0700

    create footer

---
+++ react-portfolio/src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+
+function Footer(){
+  return (
+    <footer className="footer fixed-bottom mt-auto py-3 text-center container">
+      <div className="container">
+        <span><i class="fab fa-gratipay"></i>Copyright. Mich.Chou</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
