14d91b31f6c8e73066c86343d9773c39366f7a69
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-25 14:42:16 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/20_React_Portfolio/precache-manifest.8417f4ed5ad6d83071fb25a6c79eb36e.js"
+  "/20_React_Portfolio/precache-manifest.5b791edb719ab15acc29b1d7c8630f3b.js"
 );
 
 self.addEventListener('message', (event) => {
