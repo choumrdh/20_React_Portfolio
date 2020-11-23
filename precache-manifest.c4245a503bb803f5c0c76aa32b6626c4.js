148166cebab5690a409d15229f712c6188766b94
Michelle Chou <chou.m.rdh@gmail.com>
2020-11-22 17:54:46 -0700

    Updates

---
+++ precache-manifest.c4245a503bb803f5c0c76aa32b6626c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f33c28f6bc5d3bda019a1b20b7f1f5b",
+    "url": "/20_React_Portfolio/index.html"
+  },
+  {
+    "revision": "2cf8973b85c76b3b99b7",
+    "url": "/20_React_Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24afb9d14d73eef4b965",
+    "url": "/20_React_Portfolio/static/css/main.34256953.chunk.css"
+  },
+  {
+    "revision": "2cf8973b85c76b3b99b7",
+    "url": "/20_React_Portfolio/static/js/2.5c8d7ed9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/20_React_Portfolio/static/js/2.5c8d7ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24afb9d14d73eef4b965",
+    "url": "/20_React_Portfolio/static/js/main.9c450121.chunk.js"
+  },
+  {
+    "revision": "4f30874aab1f7953cb12",
+    "url": "/20_React_Portfolio/static/js/runtime-main.2518d4f2.js"
+  },
+  {
+    "revision": "d3f2a985ba6ecba7456d1cbaf1cb3a16",
+    "url": "/20_React_Portfolio/static/media/Mchou_Resume2020.d3f2a985.pdf"
+  }
+]);
